fcd829ce4a7927614ccd62fa95a5a2a020ee6b80
asvindra <aswin1399904@gmail.com>
2021-11-20 16:32:53 +0530

    Updated Expanse tracker

--- block-BNaafu/Expanse-tracker/models/user.js
+++ block-BNaafu/Expanse-tracker/models/user.js
@@ -6,7 +6,7 @@ var bcrypt=require('bcrypt');
 var userSchema=new Schema({
     name:{type:String,required:true},
     email:{type:String,unique:true,required:true},
-    password:{type:String,required:true,minlength:5},
+    password:{type:String,minlength:5},
     age:Number,
     phone:Number,
     country:String,
--- block-BNaafu/Expanse-tracker/modules/passport.js
+++ block-BNaafu/Expanse-tracker/modules/passport.js
@@ -9,7 +9,7 @@ passport.use(new GitHubStrategy({
     clientSecret:process.env.CLIENT_SECRET,
     callbackURL:'/auth/github/callback'
 },(accessToken,refreshToken,profile,done)=>{
-console.log(profile);
+//console.log(profile);
 var profileData={
     name:profile.displayName,
     //username:profile.username,
@@ -17,16 +17,16 @@ var profileData={
     photo:profile._json.avatar_url
 }
 User.findOne({email:profile._json.email},(err,user)=>{
-   // console.log('in user',user);
+    //console.log('in git hub user',err,user);
    //req.session.userId = user.id;
     if(err)return done(err);
     if(!user){
         
         User.create(profileData,(err,addedUser)=>{
-            //console.log('in error',user);
+           // console.log('in error',err);
             if(err)return done(err);
             // console.log('inside create');
-            // console.log(addedUser);
+            // console.log('added user in git hub',addedUser);
             return done(null,addedUser);
         })
     }
@@ -43,7 +43,7 @@ passport.use(new GoogleStrategy({
     clientSecret:process.env.google_CLIENT_SECRET,
     callbackURL: '/auth/google/callback',
 },(accessToken,refreshToken,profile,done)=>{
-console.log(profile);
+//console.log(profile);
 var profileData={
     name:profile.displayName,
     //username:profile.username,
@@ -52,14 +52,15 @@ var profileData={
 }
 User.findOne({email:profile._json.email},(err,user)=>{
    // req.session.userId = user.id;
+  // console.log('in google user',err,user);
     if(err)return done(err);
     if(!user){
         
         User.create(profileData,(err,addedUser)=>{
-            console.log('in error',err);
+           // console.log('in error',err);
             if(err)return done(err);
             // console.log('inside create');
-            // console.log(addedUser);
+             //console.log('added user',err,addedUser);
             return done(null,addedUser);
         })
     }
@@ -77,7 +78,7 @@ passport.serializeUser((user,done)=>{
 })
 
 passport.deserializeUser((id,done)=>{
-    User.findById(id,'name email username',(err,user)=>{
+    User.findById(id,'name email',(err,user)=>{
         done(err,user)
     })
 
